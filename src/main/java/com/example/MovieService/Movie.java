cf211b308979f72c7b2a40b4e259e1605c2bfc57
s27814-pj <s27814@pjwstk.edu.pl>
2024-06-15 11:23:09 +0200

    zjazd7 - docs

--- src/main/java/com/example/MovieService/Movie.java
+++ src/main/java/com/example/MovieService/Movie.java
@@ -1,16 +1,21 @@
 package com.example.MovieService;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 
 @Entity
 public class Movie {
+    @Schema(name = "ID filmu", example = "1", required = true)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Schema(name = "nazwa filmu", example = "star gate", required = false)
     private String name;
+    @Schema(name = "kategoria filmu z enum", example = "SCI_FI", required = false)
     @Enumerated(EnumType.STRING)
     private Category category;
 
+    @Schema(name = "dostepnosc", example = "false", required = false)
     private boolean isAvailable = false;
 
     public Movie(Long id, String name, Category category) {
--- src/main/java/com/example/MovieService/MovieController.java
+++ src/main/java/com/example/MovieService/MovieController.java
@@ -1,5 +1,9 @@
 package com.example.MovieService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,11 +23,20 @@ public class MovieController {
         this.movieService = movieService;
     }
 
+    @Operation(summary = "Otrzmyaj filmy", description = "Wyświela listy wszystkich filmów w bazie danych")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved")
+    })
     @GetMapping("/movies")
     public ResponseEntity<List<Movie>> getMovieList(){
         return ResponseEntity.ok(movieService.getMovieList());
     }
 
+    @Operation(summary = "Pokaz film", description = "Wyświela film o podanym id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "nie ma filmu o tym id")
+    })
     @GetMapping("/movies/{id}")
     public ResponseEntity<Movie> getMovieById(@PathVariable(required = true) int id){
         return ResponseEntity.of(movieService.findMovieById(id));
@@ -35,31 +48,60 @@ public class MovieController {
 //        //return ResponseEntity.of(movieService.findMovieById(1));
 //    }
 
+
+    @Operation(summary = "dodaj film", description = "dodaje film o podanych wartosciach w POST i  zwraca go przy sukcesie")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "film dodany")
+    })
     @PostMapping("/movies")
     public ResponseEntity<Movie> addMovie(@RequestBody Movie movie) {
         return ResponseEntity.ok(movieService.addMovie(new Movie(0L, movie.getName(), movie.getCategory())));
     }
 
+    @Operation(summary = "aktualizuj film", description = "aktualizuje film z id o podane wartosci i zwraca go przy sukcesie")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "film zaktualizowany"),
+            @ApiResponse(responseCode = "404", description = "nie znaleziono filmu do aktualiacji")
+    })
     @PutMapping("/movies/{id}")
     public ResponseEntity<Movie> updateMovie(@RequestBody Movie movie, @PathVariable(required = true) int id){
         return ResponseEntity.ok(movieService.updateMovie(movie,id));
     }
-
+    @Operation(summary = "usun film", description = "usun film z id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "film usuniety"),
+            @ApiResponse(responseCode = "404", description = "nie znaleziono filmu do aktualiacji")
+    })
     @DeleteMapping("movies/{id}")
-    public ResponseEntity<Void> deleteMovie(@PathVariable(required = true) int id){
+    public ResponseEntity<Void> deleteMovie(@PathVariable(required = true) @Parameter(name = "id", description = "id filmu w bazie", example = "1") int id){
         movieService.deleteMovie(id);
         return ResponseEntity.noContent().build();
     }
+
+    @Operation(summary = "film avaiblity ustaw na true", description = "film avaiblity ustaw na true")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "film ma true"),
+            @ApiResponse(responseCode = "404", description = "nie znaleziono filmu do aktualiacji")
+    })
     @PutMapping("/setTrue/{id}")
     public ResponseEntity<Movie> setTrue(@PathVariable int id){
     return ResponseEntity.ok(movieService.setTrue(id));
     }
 
+    @Operation(summary = "film avaiblity ustaw na false", description = "film avaiblity ustaw na false")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "film ma false"),
+            @ApiResponse(responseCode = "404", description = "nie znaleziono filmu do aktualiacji")
+    })
     @PutMapping("/setFalse/{id}")
     public ResponseEntity<Movie> setFalse(@PathVariable int id){
         return ResponseEntity.ok(movieService.setFalse(id));
     }
 
+    @Operation(summary = "rzuca błędem 500", description = "rzuca błędem 500")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "500", description = "rzuca błędem 500"),
+    })
     @GetMapping("/throw500")
     public ResponseEntity<Exception> throw500(){
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
